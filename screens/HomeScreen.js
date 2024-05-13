9746894dff850679e0ca05e99b63c699933f03dc
BalogunVictor <balogunmoyinoluwav@gmail.com>
2024-05-13 19:10:02 +0100

    HomeScreen UI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,17 @@ import React from "react";
 import { Image, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAP_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import {
+  setDestination,
+  setOrigin,
+  selectTravelTimeInformation,
+} from "../slices/navSlice";
 
-const HomeScreen = ({}) => {
+const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView>
       <View className="p-5">
@@ -16,6 +25,35 @@ const HomeScreen = ({}) => {
           source={{
             uri: "https://links.papareact.com/gzs",
           }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where from"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAP_API_KEY,
+            language: "en",
+          }}
+          debounce={400}
         />
         <NavOptions />
       </View>
